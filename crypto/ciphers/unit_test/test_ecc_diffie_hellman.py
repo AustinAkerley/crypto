818953cecdd0ae498c3b7424674c4be58ede5713
AustinAkerley <aakerley@tamu.edu>
2019-12-28 15:33:02 -0700

    fixed some probs in ecc DH test and code

--- crypto/ciphers/unit_test/test_ecc_diffie_hellman.py
+++ crypto/ciphers/unit_test/test_ecc_diffie_hellman.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 from crypto.ecc.curve import curve
 from crypto.ciphers.ecc_diffie_hellman import ecc_diffie_hellman
@@ -7,12 +8,17 @@ class Test(unittest.TestCase):
     def test_ecc_diffie_1(self):
         P = (920, 303)
         E = curve(324, 1287, 3851)
-        
+
         diffie_A = ecc_diffie_hellman(E, P)
-        diffie_A.public_keygen(private_key=1194)
-        
+        QA = diffie_A.public_keygen(private_key=1194)
+        symmetric_key = diffie_A.symmetric_keygen()
+
         diffie_B = ecc_diffie_hellman(E, P)
-        diffie_B.public_keygen(private_key=1759)
+        QB = diffie_B.public_keygen(private_key=1759)
+
+        key_A = diffie_A.symmetric_keygen(QB)
+        key_B = diffie_B.symmetric_keygen(QA)
+        self.assertEqual(key_A, key_B)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
