c2789c8086905f69e0eaa577fc6d23c6bfd506e2
Austin <aakerley@tamu.edu>
2020-02-10 23:27:55 -0700

    Completed the pohlig hellman discrete logarithm solver

--- crypto/cryptanalysis/unit_test/test_pohlig_hellman.py
+++ crypto/cryptanalysis/unit_test/test_pohlig_hellman.py
@@ -11,14 +11,29 @@ import unittest
 from crypto.cryptanalysis.pohlig_hellman import pohlig_hellman
 
 class TestPohligHellman(unittest.TestCase):
+    def test_pohlig_hellman_1(self):
+        prime = 31 # 5 digit weak prime
+        g = 2
+        h = 8
+        xs= pohlig_hellman(g, h, prime)
+        expected_x = 13
+        self.assertIn(expected_x, xs)
 
-    def test_pohlig_hellman_functional_2(self):
+    def test_pohlig_hellman_2(self):
         prime = 15377 # 5 digit weak prime
         g = 3
-        h = 13575
-        x = pohlig_hellman(g, h, prime)
+        h = 13572
+        xs = pohlig_hellman(g, h, prime)
         expected_x = 6025
-        self.assertEqual(x, expected_x)
+        self.assertIn(expected_x, xs)
+
+    def test_pohlig_hellman_3(self):
+        prime = 11251 # 5 digit weak prime
+        g = 23
+        h = 9689
+        xs = pohlig_hellman(g, h, prime)
+        expected_x = 4261
+        self.assertIn(expected_x, xs)
 
 if __name__ == '__main__':
     unittest.main()
