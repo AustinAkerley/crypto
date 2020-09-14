903ee54d8a3cf6cb070062e17a55dde6659672d7
Austin <aakerley@tamu.edu>
2020-09-13 21:57:49 -0600

    testing jenkins... stilll

--- crypto/signatures/unit_test/test_rsa_sig.py
+++ crypto/signatures/unit_test/test_rsa_sig.py
@@ -19,7 +19,7 @@ class TestRSASignature(unittest.TestCase):
         new_signature = rsa_sig(p, q)
         new_signature.gen_e()
         e = new_signature.e
-        signature = new_signature.sign("./crypto/signatures/unit_test/example_doc.txt") 
+        signature = new_signature.sign("./crypto/signatures/unit_test/example_doc.txt")
 
         signature_verifier = rsa_sig()
         is_signature_valid = signature_verifier.verify(signature, "example_doc.txt", e, N)
@@ -27,7 +27,7 @@ class TestRSASignature(unittest.TestCase):
 
     def test_rsa_signature_2(self):
         print("\n\nRunning test for cipher: rsa_sig")
-        p = random_prime(128)
+        p = random_prime(128) 
         q = random_prime(128)
         N = p*q
         new_signature = rsa_sig(p, q)
